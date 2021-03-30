b881889ee555ccc5c4d2624fadc66b1ab846b578
mjeshan <mahathir.jeshan@aivalabs.com>
2021-03-31 01:26:06 +0600

    glob sync updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import path from "path";
 import glob from "glob";
-const htmlFiles = glob.sync(path.resolve(__dirname, "src", "*.html"));
+
 export default {
   root: path.join(__dirname, "src"),
   build: {
     outDir: path.join(__dirname, "dist"),
     rollupOptions: {
-      input: htmlFiles,
+      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
     },
   },
 };
