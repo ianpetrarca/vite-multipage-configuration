965167318dabb612968ca48df68d477bd7940c2c
mjeshan <mahathir.jeshan@aivalabs.com>
2021-03-30 22:49:40 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# vite-multipage-configuration (vanilla js)
+First:
+```
+npm install
+```
+
+Run in Dev:
+```
+npm run dev
+```
+
+Build:
+```
+npm run build
+```
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from "path";
+export default {
+  root: path.join(__dirname, "src"),
+  build: {
+    outDir: path.join(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+      },
+    },
+  },
+};
